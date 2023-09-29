57f3cd3fe243ebe6d33020601a7f9be7758bc8a9
KatiaLosignore <katialos.kl@gmail.com>
2023-09-29 13:25:37 +0200

    added Database e done migrations and update

---
+++ la-mia-pizzeria-static/Database/PizzaContext.cs
@@ -0,0 +1,17 @@
+﻿using la_mia_pizzeria_static.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace la_mia_pizzeria_static.Database
+{
+    public class PizzaContext : DbContext
+    {
+        public DbSet<Pizza> Pizzas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=MyPizzeria2023;Integrated Security=True;TrustServerCertificate=True");
+        }
+
+
+    }
+}
