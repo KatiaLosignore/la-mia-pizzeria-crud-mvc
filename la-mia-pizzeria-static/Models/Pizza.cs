c2c404e65837459f561f1ee74cf683fa1bdbf278
KatiaLosignore <katialos.kl@gmail.com>
2023-10-02 16:24:14 +0200

    added button create pizza

--- la-mia-pizzeria-static/Models/Pizza.cs
+++ la-mia-pizzeria-static/Models/Pizza.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace la_mia_pizzeria_static.Models
@@ -7,14 +8,21 @@ namespace la_mia_pizzeria_static.Models
     {
         public int Id { get; set; }
 
-        [MaxLength(150)]
+        [Required(ErrorMessage = "Il nome della pizza è obbligatorio!")]
+        [MaxLength(150, ErrorMessage = "La massima lunghezza del nome è di 150 caratteri")]
         public string Name { get; set; }
 
         [Column(TypeName = "text")]
+        [Required(ErrorMessage = "La descrizione della pizza è obbligatoria!")]
         public string Description { get; set; }
+
+        [Required(ErrorMessage = "Il prezzo della pizza è obbligatorio!")]
+        [Range(1, 100, ErrorMessage = "Il prezzo di una pizza deve variare da € 1 a € 100")]
         public float Price { get; set; }
 
-        [MaxLength(500)]
+        [MaxLength(500, ErrorMessage = "La massima lunghezza del link è di 500 caratteri")]
+        [Url(ErrorMessage = "Inserisci un url valido")]
+        [DefaultValue("/img/default.jpg")]
         public string Image { get; set; }
 
         public Pizza() { }
