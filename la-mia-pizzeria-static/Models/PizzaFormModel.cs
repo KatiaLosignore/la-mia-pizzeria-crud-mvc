c95c1045b7d88bd2c6dc4f541709dcb50d759ab7
KatiaLosignore <katialos.kl@gmail.com>
2023-10-04 12:16:13 +0200

    inserted the data into the database and create PizzaFormModel

---
+++ la-mia-pizzeria-static/Models/PizzaFormModel.cs
@@ -0,0 +1,10 @@
+﻿using la_mia_pizzeria_static.Models.Database_Models;
+
+namespace la_mia_pizzeria_static.Models
+{
+    public class PizzaFormModel
+    {
+        public Pizza Pizza { get; set; }
+        public List<Category>? Categories { get; set; }
+    }
+}
