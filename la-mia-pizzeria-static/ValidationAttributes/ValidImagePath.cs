6ee16584be18c3aaea4da4fc5f1f7581009d15b7
KatiaLosignore <katialos.kl@gmail.com>
2023-10-04 17:11:24 +0200

    added fixed

--- la-mia-pizzeria-static/ValidationAttributes/ValidImagePath.cs
+++ la-mia-pizzeria-static/ValidationAttributes/ValidImagePath.cs
@@ -10,7 +10,7 @@ namespace la_mia_pizzeria_static.ValidationAttributes
         {
             var imgext = Path.GetExtension((string)value);
 
-            if (imgext != ".jpg" && imgext != ".png")
+            if (imgext != ".jpg" && imgext != ".png" && imgext != ".webp")
                 return new ValidationResult("Puoi inserire solo immagini di tipo \".jpg\" e \".png\"");
             return ValidationResult.Success;
         }
