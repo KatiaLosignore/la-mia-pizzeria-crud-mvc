1866b25baaa756c97d7e131b07a4998f12de70ff
KatiaLosignore <katialos.kl@gmail.com>
2023-10-05 13:06:38 +0200

    added properties in PizzaFormModel

--- la-mia-pizzeria-static/Models/PizzaFormModel.cs
+++ la-mia-pizzeria-static/Models/PizzaFormModel.cs
@@ -1,4 +1,5 @@
 ﻿using la_mia_pizzeria_static.Models.Database_Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace la_mia_pizzeria_static.Models
 {
@@ -6,5 +7,10 @@ namespace la_mia_pizzeria_static.Models
     {
         public Pizza Pizza { get; set; }
         public List<Category>? Categories { get; set; }
+
+
+        // Inserisco nuove proprietà per gestire una select multipla nelle viste
+        public List<SelectListItem>? Ingredients { get; set; }
+        public List<string>? SelectedIngredientsId { get; set; }
     }
 }
